552b97718b7ef03d73b4320ae98bf0c65151dcca
Seu Nome <jtiagolimac@gmail.com>
2025-11-03 09:03:59 -0300

    Parte inicial do cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./Components/Login/Login";
+import Cadastro from "./Components/Cadastro/cadastro.jsx";
 import Inicio from "./Components/Inicio/Inicio";
 
 function App() {
@@ -10,7 +11,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Login />} />
+
           <Route path="/home" element={<Inicio />} />
+
+          <Route path="/cadastro" element={<Cadastro />} />
         </Routes>
       </div>
     </Router>
--- src/Components/Cadastro/Cadastro.jsx
+++ src/Components/Cadastro/Cadastro.jsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import { FaUser, FaLock } from "react-icons/fa";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Cadastro() {
-  return <h1>Faça o cadastro.</h1>;
-}
+import "./Cadastro.css";
+
+const Cadastro = () => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [perfil, setPerfil] = useState('aluno');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    console.log('Dados do Cadastro:', {
+      email,
+      senha,
+      perfil,
+    });
+  };
+
+  return (
+    <div className="cadastro-page">
+      <div className="cadastro-container">
+        <h2>Cadastro</h2>
+        <form onSubmit={handleSubmit}>
+          
+          <div className="form-group">
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="password"
+              id="senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="perfil">Tipo de Perfil</label>
+            <select
+              id="perfil"
+              value={perfil}
+              onChange={(e) => setPerfil(e.target.value)}
+              required
+            >
+              <option value="aluno">Aluno</option>
+              <option value="professor">Professor</option>
+              <option value="gestor">Gestor</option>
+            </select>
+          </div>
+          
+          <button type="submit" className="submit-btn">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Cadastro;
